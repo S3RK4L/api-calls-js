4ab5e42a3e01016f0040736c180bc5d12d4a5f8b
Serkal <kali.hinder@ultixtechnology.com>
2024-10-10 23:25:00 +0100

    removed button and added a dropdown that displays the name of every country from the api

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById('displayCountriesBtn').addEventListener('click', retrieveCountryData);
-
 // communicates to country api to retrieve all country data to display it on the page
 async function retrieveCountryData(){
     var apiUrl = "https://restcountries.com/v3.1/all";
@@ -12,13 +10,41 @@ async function retrieveCountryData(){
         })
         .then(data => {
             console.log(data); // for testing
-            displayData(data);
+            //displayData(data);
+            populateCountriesDropdown(data);
         })
         .catch(error => {
             console.error('Error with fetch: ', error);
     });
 }
 
+//test function for displaying whole output of api request
 function displayData(data){
     document.getElementById('apiOutput').textContent = JSON.stringify(data, null, 2);
 }
+
+//populate countries dropdown list
+function populateCountriesDropdown(countriesData){
+    const select = document.getElementById('countriesDropDown');
+
+    countriesData.forEach(country => {
+        const opt = document.createElement('option');
+        opt.value = country.name.common;
+        opt.innerHTML = country.name.common;
+        select.appendChild(opt);
+    });
+
+    select.addEventListener('change', function() {
+        handleCountrySelection(select.value);
+    });
+}
+
+function handleCountrySelection(countryName){
+    console.log(`Selected country ID: ${countryName}`);
+}
+
+//document.getElementById('displayCountriesBtn').addEventListener('click', retrieveCountryData);
+// Run this function when the page loads to fetch and display the country data
+document.addEventListener('DOMContentLoaded', function() {
+    retrieveCountryData();
+});
